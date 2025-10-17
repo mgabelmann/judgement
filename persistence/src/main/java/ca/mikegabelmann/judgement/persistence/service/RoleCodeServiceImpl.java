d5a6cd01473842b3d08ddee5767543d7fc2aaa07
mgabelmann <mgabelmann@gmail.com>
2025-10-17 10:27:10 -0700

    Added service and tests to support it

---
+++ persistence/src/main/java/ca/mikegabelmann/judgement/persistence/service/RoleCodeServiceImpl.java
@@ -0,0 +1,35 @@
+package ca.mikegabelmann.judgement.persistence.service;
+
+import ca.mikegabelmann.judgement.persistence.model.RoleCode;
+import ca.mikegabelmann.judgement.persistence.repository.RoleCodeRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class RoleCodeServiceImpl implements RoleCodeService {
+    /** Logger. */
+    private static final Logger LOG = LoggerFactory.getLogger(RoleCodeServiceImpl.class);
+
+    private RoleCodeRepository roleCodeRepository;
+
+    @Autowired
+    public RoleCodeServiceImpl(RoleCodeRepository roleCodeRepository) {
+        this.roleCodeRepository = roleCodeRepository;
+    }
+
+    @Override
+    public List<RoleCode> findAll() {
+        return roleCodeRepository.findAll();
+    }
+
+    @Override
+    public Optional<RoleCode> findById(String id) {
+        return roleCodeRepository.findById(id);
+    }
+
+}
---
+++ persistence/src/test/java/ca/mikegabelmann/judgement/persistence/service/RoleCodeServiceImplTest.java
@@ -0,0 +1,52 @@
+package ca.mikegabelmann.judgement.persistence.service;
+
+import ca.mikegabelmann.judgement.persistence.model.RoleCode;
+import ca.mikegabelmann.judgement.persistence.repository.RoleCodeRepository;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+
+@ExtendWith(MockitoExtension.class)
+class RoleCodeServiceImplTest {
+    @Mock
+    private RoleCodeRepository roleCodeRepository;
+
+    @InjectMocks
+    private RoleCodeServiceImpl roleCodeService;
+
+    private RoleCode roleCode1;
+    private RoleCode roleCode2;
+
+
+    @BeforeEach
+    void beforeEach() {
+        this.roleCode1 = new RoleCode(true, "description", "ADMIN");
+        this.roleCode2 = new RoleCode(false, "description", "USER_OLD");
+    }
+
+    @Test
+    void findAll() {
+        Mockito.when(roleCodeRepository.findAll()).thenReturn(Arrays.asList(roleCode1, roleCode2));
+        List<RoleCode> roleCodes = roleCodeService.findAll();
+        Assertions.assertNotNull(roleCodes);
+        Assertions.assertEquals(2, roleCodes.size());
+    }
+
+    @Test
+    void findById() {
+        Mockito.when(roleCodeRepository.findById(roleCode1.getId())).thenReturn(Optional.of(roleCode1));
+        Optional<RoleCode> roleCode = roleCodeService.findById(roleCode1.getId());
+        Assertions.assertNotNull(roleCode);
+        Assertions.assertTrue(roleCode.isPresent());
+    }
+
+}
